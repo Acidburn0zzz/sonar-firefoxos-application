e748e963b1c29ac3290c71aea3a798dfea063b9b
bakaibalazs <bakaibalazs@gmail.com>
2013-12-14 12:58:11 +0100

    fix drawer-main content issue

--- js/app.js
+++ js/app.js
@@ -1,57 +1,100 @@
-var xhr;
-
-$(document).ready(function() {
-  $("#buttonProjects").click(function() {
-    sendXHR("GET", "http://nemo.sonarqube.org/api/resources?format=json", processProjects());
-  });
-
-  $("#buttonPlugins").click(function() {
-    sendXHR("GET", "http://nemo.sonarqube.org/api/updatecenter/installed_plugins?format=json", processPlugins());
-  });
-
-  $("#buttonUsers").click(function() {
-    sendXHR("GET", "http://nemo.sonarqube.org/api/users/search?format=json", processUsers());
-  });
+var xhr = new XMLHttpRequest({
+  mozSystem : true
 });
 
 function sendXHR(requestType, requestURL, handler) {
-  xhr = new XMLHttpRequest({
-    mozSystem : true
-  });
-  xhr.onreadystatechange = handler;
+
+  xhr.onreadystatechange = handler; // xhr.onload = handler;
   xhr.open(requestType, requestURL, true);
+  xhr.onerror = function() {
+    console.log('Error loading data');
+  };
+
   xhr.send();
 }
 
+$(document).ready(function() {
+
+  populateProjectsTab();
+
+  $("#buttonProjects").click(function() {
+    populateProjectsTab();
+  });
+
+  $("#buttonPlugins").click(function() {
+    populatePluginsTab();
+  });
+
+  $("#buttonUsers").click(function() {
+    populateUsersTab()
+  });
+});
+
+function populateProjectsTab() {
+  console.log("populateProjectsTab");
+
+  $("#resultsProjects").css("visibility", "visible");
+  $("#resultsPlugins").css("visibility", "hidden");
+  $("#resultsUsers").css("visibility", "hidden");
+  sendXHR("GET", "http://nemo.sonarqube.org/api/resources?format=json", processProjects());
+}
+
+function populatePluginsTab() {
+  console.log("populatePluginsTab");
+
+  $("#resultsProjects").css("visibility", "hidden");
+  $("#resultsPlugins").css("visibility", "visible");
+  $("#resultsUsers").css("visibility", "hidden");
+  sendXHR("GET", "http://nemo.sonarqube.org/api/updatecenter/installed_plugins?format=json", processPlugins());
+}
+
+function populateUsersTab() {
+  console.log("populateUsersTab");
+  $("#resultsProjects").css("visibility", "hidden");
+  $("#resultsPlugins").css("visibility", "hidden");
+  $("#resultsUsers").css("visibility", "visible");
+  sendXHR("GET", "http://nemo.sonarqube.org/api/users/search?format=json", processUsers());
+}
+
 function processProjects() {
+  console.log("in process projects function: " + xhr.readyStat + " -- " + xhr.status);
   return function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
       var obj = jQuery.parseJSON(xhr.responseText);
       for (var i = 0; i < obj.length; i++) {
         $('#resultsProjects').append("<li><p>" + obj[i].name + "</p><p>" + obj[i].lang + "</p></li>");
       }
+    } else {
+      console.log("did not get data " + xhr.status);
     }
   }
 }
 
 function processUsers() {
+  console.log("in process users function: " + xhr.readyStat + " -- " + xhr.status);
   return function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
       var obj = jQuery.parseJSON(xhr.responseText);
       for (var i = 0; i < obj['users'].length; i++) {
-        $('#resultsUsers').append("<li><p>" + obj['users'][i].name + "</p><p>" + (obj['users'][i].email == null ? '' : obj['users'][i].email) + "</p></li>");
+        $('#resultsUsers').append(
+            "<li><p>" + obj['users'][i].name + "</p><p>" + (obj['users'][i].email == null ? '' : obj['users'][i].email) + "</p></li>");
       }
+    } else {
+      console.log("did not get data " + xhr.status);
     }
   }
 }
 
 function processPlugins() {
+  console.log("in process plugins function: " + xhr.readyStat + " -- " + xhr.status);
   return function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
       var obj = jQuery.parseJSON(xhr.responseText);
       for (var i = 0; i < obj.length; i++) {
         $('#resultsPlugins').append("<li><p>" + obj[i].name + "</p><p>" + obj[i].version + "</p></li>");
       }
+    } else {
+      console.log("did not get data " + xhr.status);
     }
   }
 }
