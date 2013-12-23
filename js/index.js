3d42e8a7494392012de4ee8e00b1dd6e5954c950
bakaibalazs <bakaibalazs@gmail.com>
2013-12-23 15:50:01 +0100

    loading progressbar added

--- js/index.js
+++ js/index.js
@@ -63,14 +63,17 @@ $(document).ready(function() {
 function refreshActiveTab(){
   if(activeTab=="projects"){
     projectsAreLoaded=false;
+    $("#resultsProjects").empty();
     populateProjectsTab();
   }
   else if(activeTab=="users"){
       usersAreLoaded=false;
+      $("#resultsUsers").empty();
       populateUsersTab();
   }
   else{
       pluginsAreLoaded=false;
+      $("#resultsPlugins").empty();
       populatePluginsTab();
     }
 }
@@ -88,8 +91,12 @@ function populateProjectsTab() {
   $("#resultsPlugins").css("visibility", "hidden");
   $("#resultsUsers").css("visibility", "hidden");
 
-  if (!projectsAreLoaded)
+  if (!projectsAreLoaded){
+  showProgressBar("#projectsProgress");
     sendXHR("GET", "http://nemo.sonarqube.org/api/resources?format=json", processProjects());
+  }
+
+
 }
 
 function populateUsersTab() {
@@ -105,8 +112,10 @@ function populateUsersTab() {
   $("#resultsPlugins").css("visibility", "hidden");
   $("#resultsUsers").css("visibility", "visible");
 
-  if (!usersAreLoaded)
+  if (!usersAreLoaded){
+    showProgressBar("#usersProgress");
     sendXHR("GET", "http://nemo.sonarqube.org/api/users/search?format=json", processUsers());
+  }
 }
 
 function populatePluginsTab() {
@@ -122,8 +131,10 @@ function populatePluginsTab() {
   $("#resultsPlugins").css("visibility", "visible");
   $("#resultsUsers").css("visibility", "hidden");
 
-  if (!pluginsAreLoaded)
+  if (!pluginsAreLoaded){
+    showProgressBar("#pluginsProgress");
     sendXHR("GET", "http://nemo.sonarqube.org/api/updatecenter/installed_plugins?format=json", processPlugins());
+  }
 }
 
 function processProjects() {
@@ -139,6 +150,7 @@ function processProjects() {
           $('#resultsProjects').append(listItem);
       }
       projectsAreLoaded = true;
+      hideProgressBar("#projectsProgress");
     } else {
       console.log("did not get data " + xhr.status);
     }
@@ -166,6 +178,7 @@ function processUsers() {
 
       }
       usersAreLoaded = true;
+      hideProgressBar("#usersProgress");
     } else {
       console.log("did not get data " + xhr.status);
     }
@@ -181,8 +194,19 @@ function processPlugins() {
         $('#resultsPlugins').append("<li><p>" + obj[i].name + "</p><p>" + obj[i].version + "</p></li>");
       }
       pluginsAreLoaded = true;
+      hideProgressBar("#pluginsProgress");
     } else {
       console.log("did not get data " + xhr.status);
     }
   }
+}
+
+function showProgressBar(id){
+ $(id).css("visibility", "visible");
+ $(id).css("display", "block");
+}
+
+function hideProgressBar(id){
+  $(id).css("visibility", "hidden");
+  $(id).css("display", "none");
 }
